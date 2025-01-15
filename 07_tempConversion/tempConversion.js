759515eeeb7bad52c2577d870394f94e6aba4109
VesperVanity <192909324+VesperVanity@users.noreply.github.com>
2025-01-15 12:33:42 -0500

    Complete exercise tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) 
+{
+  let celsius = 0;
+  celsius = (fahrenheit - 32) * 5/9;
+  let celsius_round = Math.round(celsius * 10) / 10;
+  return celsius_round;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) 
+{
+  let fahrenheit = 0;
+  fahrenheit = (celsius * 9/5) + 32;
+  let fahrenheit_round = Math.round(fahrenheit * 10) / 10;
+  return fahrenheit_round;
 };
 
 // Do not edit below this line
