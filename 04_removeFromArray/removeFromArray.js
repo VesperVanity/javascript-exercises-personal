98f02586e27faaf59d61fba597308c9c22439abd
VesperVanity <192909324+VesperVanity@users.noreply.github.com>
2025-01-15 09:27:09 -0500

    Complete exercise removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) 
+{
+    const new_array = [];
+    array.forEach(element => {
+        if(!args.includes(element))
+        {
+            new_array.push(element);
+        }
+    });
+    return new_array;
 };
 
 // Do not edit below this line
