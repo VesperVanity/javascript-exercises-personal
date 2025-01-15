cd96e78b293f822b63614fc9902e19f25c3ad8eb
VesperVanity <192909324+VesperVanity@users.noreply.github.com>
2025-01-15 08:58:21 -0500

    Complete 01_helloWorld and 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,20 @@
-const repeatString = function() {
-
+const repeatString = function( string, num) 
+{
+    let empty_string = "";
+    if(num >= 0)
+    {
+        for(let i = 0; i < num; i++)
+            {
+                empty_string += string;
+            }
+            return empty_string;
+    }
+    else
+    {
+        empty_string = "ERROR";
+        return empty_string;
+    }
+    
 };
 
 // Do not edit below this line
